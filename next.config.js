4c3bab38bf3c6f1574c0c15275992cf3e5b7548c
sgbo5003 <sgbo5003@github.com>
2024-03-11 00:23:09 +0900

    next config 파일 수정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const repository = 'http://sgbo5003.github.io/my-portfolio';
-const debug = process.env.NODE_ENV !== 'production';
-const prefix = process.env.NODE_ENV === 'production' ? 'http://sgbo5003.github.io/my-portfolio' : '';
-const nextConfig = {
-  assetPrefix: !debug ? `/${repository}/` : '', // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
